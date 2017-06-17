122919bbb30ac5abd104d38cb000e029fef49bf7
Usua Madina <u.madina.i@gmail.com>
2017-06-17 21:27:51 +0200

    fixing errors on newRoute

--- js/views/newRoute.js
+++ js/views/newRoute.js
@@ -6,12 +6,12 @@ var NewRoute = Backbone.View.extend({
         this.timerGps = null;
         this.clockCounter = null;
         this.render();
-    }
+    },
     startRoute: function() {
         this.model = new Route();
         console.log('startRoute(' + this.model.id + ')');
         // recuperar título de la ruta
-        var title = this.$('#txttitle').val();
+        var title = this.$('#txtTitulo').val();
         if (!title || title.length == 0) title = "Default";
         this.model.set("title", title);
         // comenzar grabación de ruta (timers, ...)
@@ -27,10 +27,10 @@ var NewRoute = Backbone.View.extend({
         }, 1000);
         // pintar vista
         this.render();
-    }
+    },
 
-        stopRoute: function() {
-        console.log('stopRoute(' + this.model.id + ')');
+        stopRoute: function() {          
+       // console.log('stopRoute(' + this.model.id + ')');
         this.recording = false;
         // parar timers
         clearInterval(this.timerClock);
@@ -41,19 +41,19 @@ var NewRoute = Backbone.View.extend({
         this.model = null;
         // pintar vista
         this.render();
-    }
+    },
 
-        leerGps: function() {
+        readGps: function() {
         // init
         window.inc = typeof(inc) == 'undefined' ? 0.00005 : window.inc;
         window.lat = typeof(lat) == 'undefined' ? 38.695015 : window.lat;
         window.lng = typeof(lng) == 'undefined' ? -0.476049 : window.lng;
-        window.dir = typeof(dir) == 'undefined' ? Math.floor((Math.random() * 4)) : window.dir; //
-        numbers 0, 1, 2, 3(0 up, 1 right, 2, down, 3 left)
+        window.dir = typeof(dir) == 'undefined' ? Math.floor((Math.random() * 4)) : window.dir; 
+        	// numbers 0, 1, 2, 3(0 up, 1 right, 2, down, 3 left)
             // generate direction (randomly)
             // it is more likely to follow the previous direction
-        var nuevaDir = Math.floor((Math.random() * 4)); // number 0,1,2,3
-        if (nuevaDir != (dir + 2) % 4) dir = nuevaDir;
+        var newDir = Math.floor((Math.random() * 4)); // number 0,1,2,3
+        if (newDir != (dir + 2) % 4) dir = newDir;
         switch (dir) {
             case 0: // up
                 lat += inc;
@@ -74,13 +74,13 @@ var NewRoute = Backbone.View.extend({
         var positions = this.model.get('positions');
         positions.push(pos);
         this.model.set('positions', positions);
-    }
+    },
 
         render: function() {
         if (this.recording) {
             this.$('#txtTitulo').val(this.model.get('title')).textinput('refresh');
-            // show panel change
-            button text
+            // show panel change button text
+
             this.$('#btGrabar').val('Parar').button('refresh');
             this.$('#lblInfo').text('Guardando ruta ' + this.model.get('title') + ' ...');
             var minutes = parseInt(this.clockCounter / 60);
@@ -91,18 +91,20 @@ var NewRoute = Backbone.View.extend({
                 (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds);
             this.$('#pnInfo').css('visibility', 'visible');
         } else {
-            // hide panel change
-            button text
+            // hide panel change button text
+            
             this.$('#btGrabar').val('Empezar ruta').button('refresh');
             this.$('#pnInfo').css('visibility', 'hidden');
 
         }
-    }
+    },
 
         events: {
         'click #btGrabar': function() {
-            if (this.recording) this.startRoute();
-            else this.stopRoute();
+            if (this.recording) {                
+                this.stopRoute();
+            }                
+            else this.startRoute();
         }
-    }
+    },
 });
